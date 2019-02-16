1d353d9390365cdc9484e69d6b70ac6fc7c579e4
afeiship <1290657123@qq.com>
2019-02-16 23:19:44 +0800

    add: v/charts

--- src/components/media/tu-charts.js
+++ src/components/media/tu-charts.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 export default class extends React.Component {
-  render(){
+  render() {
     const { src, description } = this.props;
     console.log(src, description);
     return (
       <figure>
-        <img src={'https://via.placeholder.com/200x100'} />
-        <p>你可以定制 TuCharts</p>
+        <iframe frameBorder="0" src="https://v.qq.com/txp/iframe/player.html?vid=g001207s5rg" allowFullScreen></iframe>
+        <p>图表/v.qq.com 原理相同</p>
       </figure>
-    )
+    );
   }
 }
